8449aabb453b77bbade98d070decbc88088361a3
Moldee81 <a.penatomczak@gmail.com>
2025-03-04 11:26:31 +0000

    Adding typewriter effect and js function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Nic dwa razy się nie zdarza",
+    autoStart: true,
+    delay: 60,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
